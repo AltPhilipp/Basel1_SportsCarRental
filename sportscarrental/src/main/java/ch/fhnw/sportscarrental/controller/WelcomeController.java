c6e9326ef71f943fbd60c3be514dc7aa9afab61a
ryan1480 <44569050+ryan1480@users.noreply.github.com>
2024-05-15 13:57:06 +0000

    bombacla

--- sportscarrental/src/main/java/ch/fhnw/sportscarrental/controller/WelcomeController.java
+++ sportscarrental/src/main/java/ch/fhnw/sportscarrental/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package ch.fhnw.sportscarrental.controller;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,12 @@ public class WelcomeController {
     @GetMapping("/")
     public String getWelcomeString() {
         return "Welcome to the Sports Car Rental Service!";
+    }
+
+    @GetMapping(value="/user")
+    public String getUserRole(org.springframework.security.core.Authentication auth) {
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        String role = userDetails.getAuthorities().toArray()[1].toString();
+        return role;
     }
 }
