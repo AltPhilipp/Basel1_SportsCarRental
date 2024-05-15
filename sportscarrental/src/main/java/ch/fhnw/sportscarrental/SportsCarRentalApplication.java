314163a53ff15e88520fb8c82a6a7f7b9f60d600
ryan1480 <44569050+ryan1480@users.noreply.github.com>
2024-05-15 13:56:06 +0000

    bomb

--- sportscarrental/src/main/java/ch/fhnw/sportscarrental/SportsCarRentalApplication.java
+++ sportscarrental/src/main/java/ch/fhnw/sportscarrental/SportsCarRentalApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.fhnw.sportscarrental.business.service.CarService;
-import ch.fhnw.sportscarrental.data.domain.Car;
+import ch.fhnw.sportscarrental.data.domain.Car; 
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.annotation.PostConstruct;
 
